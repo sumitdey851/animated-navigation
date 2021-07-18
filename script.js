1052d1229a6a533be7e15df3b33132537c45c1f4
sumitdey851 <sumitdey851@outlook.com>
2021-07-18 15:46:20 +0530

    Code Optimization

--- script.js
+++ script.js
@@ -5,7 +5,13 @@ const menuItems = [];
 for (let i = 0; i < 5; i++) {
     menuItems[i] = document.getElementById(`nav-${i+1}`);
 };
-console.log(menuItems);
+
+// Control Navigation Animation
+function navAnimation(direction1, direction2) {
+    menuItems.forEach((nav, i) => {
+        nav.classList.replace(`slide-${direction1}-${i+1}`, `slide-${direction2}-${i+1}`);
+    });
+}
 
 function toggleNav() {
     // Toggle: Menu Bars Open/Close
@@ -14,22 +20,14 @@ function toggleNav() {
     overlay.classList.toggle('overlay-active');
     if (overlay.classList.contains('overlay-active')) {
         // Animate In - Overlay
-        overlay.classList.remove('overlay-slide-left');
-        overlay.classList.add('overlay-slide-right');
+        overlay.classList.replace('overlay-slide-left', 'overlay-slide-right');
         // Animate In - Nav Items
-        for (let i = 0; i < 5; i++) {
-            menuItems[i].classList.remove(`slide-out-${i+1}`);
-            menuItems[i].classList.add(`slide-in-${i+1}`);
-        };
+        navAnimation('out', 'in');
     } else {
         // Animate Out - Overlay
-        overlay.classList.remove('overlay-slide-right');
-        overlay.classList.add('overlay-slide-left');
+        overlay.classList.replace('overlay-slide-right', 'overlay-slide-left');
         // Animate Out - Nav Items
-        for (let i = 0; i < 5; i++) {
-            menuItems[i].classList.remove(`slide-in-${i+1}`);
-            menuItems[i].classList.add(`slide-out-${i+1}`);
-        };
+        navAnimation('in', 'out');
     }
 }
 
@@ -38,4 +36,3 @@ menuBars.addEventListener('click', toggleNav);
 for (let i = 0; i < 5; i++) {
     menuItems[i].addEventListener('click', toggleNav);
 };
-
